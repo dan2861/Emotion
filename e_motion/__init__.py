070b973d4dabc3e6d2145993379b014f0beb17ce
Daniel Yonas <danu@Daniels-MacBook-Air.local>
2021-12-11 14:48:08 -0500

    Setup skeleton watch blueprint and html rendering with video list.

--- e_motion/__init__.py
+++ e_motion/__init__.py
@@ -18,27 +18,19 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/')
-    def index():
-        from flask import render_template
-        return render_template('index.html')
-
     # register the database commands
     from e_motion import db
 
     db.init_app(app)
 
-    # # apply the blueprints to the app
+    # apply the blueprints to the app
     # from flaskr import auth, blog
+    from e_motion import watch
 
     # app.register_blueprint(auth.bp)
-    # app.register_blueprint(blog.bp)
+    app.register_blueprint(watch.bp)
 
-    # make url_for('index') == url_for('blog.index')
-    # in another app, you might define a separate main index here with
-    # app.route, while giving the blog blueprint a url_prefix, but for
-    # the tutorial the blog will be the main index
-    # app.add_url_rule("/", endpoint="index")
+    # make url_for('index') == url_for('watch.index')
+    app.add_url_rule("/", endpoint="index")
 
     return app
