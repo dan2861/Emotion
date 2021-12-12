ad4d00548324780161f61361f678e2927d5a02d6
Daniel Yonas <danu@Daniels-MacBook-Air.local>
2021-12-12 06:20:17 -0500

    Templated the carousel.

--- e_motion/watch.py
+++ e_motion/watch.py
@@ -11,12 +11,14 @@ def index():
     """Populate the relevant video files for the current user."""
     # Use db once it is intialized
     # db = get_db()
-    top_section_videos = [
+    recent_videos = [
     	"https://www.youtube.com/embed/6hgVihWjK2c",
     	"https://player.vimeo.com/video/84910153?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff",
     	"https://www.youtube.com/embed/oiKj0Z_Xnjc",
     	# Local uploaded video
     	url_for('static', filename='assets/videos/video1.mp4'),
-    	url_for('static', filename='assets/videos/video2.mp4')
+    	url_for('static', filename='assets/videos/video2.mp4'),
+    	# TODO: If upload youtube link convert from watch to embed.
+    	"https://www.youtube.com/embed/Dd7FixvoKBw",
     ]
-    return render_template("index.html", top_vids=top_section_videos)
+    return render_template("index.html", recent_vids=recent_videos,title="Dani's Template", subtitle="The temppate Subtitle")
