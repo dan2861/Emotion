cf668f336de49f651f5130d23d708780177c7946
Daniel Yonas <danu@Daniels-MacBook-Air.local>
2021-12-12 11:33:44 -0500

    Videos are now paused on start

--- e_motion/watch.py
+++ e_motion/watch.py
@@ -11,33 +11,39 @@ all_videos = [
     	"url" : "https://www.youtube.com/embed/6hgVihWjK2c",
     	"title" : "Radio Head Jonny Tom",
     	"description": "",
+        "type" : "embed"
     },
     { 
     	"url" : "https://player.vimeo.com/video/84910153?title=0&amp;byline=0&amp;portrait=0&amp;badge=0&amp;color=ffffff",
     	"title" : "Kewti Animation",
     	"description": "This is deep kewti animation by dani",
+        "type" : "embed"
     },
     { 
     	"url" : "https://www.youtube.com/embed/oiKj0Z_Xnjc",
     	"title" : "Stromae - Papouti",
     	"description": "",
+        "type" : "embed"
     },
     # Local uploaded video
     { 
     	"url" : "/static/assets/videos/video1.mp4",
     	"title" : "Random Lady",
     	"description": "A nice video by random lady",
+        "type" : "mp4"
     },
     { 
     	"url" : "/static/assets/videos/video2.mp4",
     	"title" : "Break Dancer",
     	"description": "",
+        "type" : "mp4"
     },
     # TODO: If upload youtube link convert from watch to embed.
     { 
     	"url" : "https://www.youtube.com/embed/Dd7FixvoKBw",
     	"title" : "Blaaake!",
     	"description": "",
+        "type" : "embed"
     },
 ]
 
@@ -46,23 +52,27 @@ recent_videos = [
     	"url" : "https://www.youtube.com/embed/6hgVihWjK2c",
     	"title" : "Radio Head Jonny Tom",
     	"description": "",
+        "type" : "embed"
     },
     # Local uploaded video
     { 
     	"url" : "/static/assets/videos/video1.mp4",
     	"title" : "Random Lady",
     	"description": "A nice video by random lady",
+        "type" : "mp4"
     },
     { 
     	"url" : "/static/assets/videos/video2.mp4",
     	"title" : "Break Dancer",
     	"description": "",
+        "type" : "mp4"
     },
     # TODO: If upload youtube link convert from watch to embed.
     { 
     	"url" : "https://www.youtube.com/embed/Dd7FixvoKBw",
     	"title" : "Blaaake!",
     	"description": "",
+        "type" : "embed"
     },
 ]
 
@@ -71,12 +81,14 @@ my_videos = [
     	"url" : "/static/assets/videos/video2.mp4",
     	"title" : "Break Dancer",
     	"description": "",
+        "type" : "mp4"
     },
     # TODO: If upload youtube link convert from watch to embed.
     { 
     	"url" : "https://www.youtube.com/embed/Dd7FixvoKBw",
     	"title" : "Blaaake!",
     	"description": "",
+        "type" : "embed"
     },
 ]
 
